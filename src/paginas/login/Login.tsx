790a92c181c52ad51ae4c3589aa3ea91d6c4ad1a
Camilla Villares <camillav-reis@hotmail.com>
2023-03-20 18:07:46 -0300

    integração do back com login

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,19 +1,44 @@
 import { Button, Grid, Typography } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import { Box } from '@mui/material';
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import { Link } from 'react-router-dom';
+import UserLogin from '../../models/UserLogin';
+
 
 
 function Login (){
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id:0,
+            usuario: '',
+            senha: '',
+            token: ''
+        })
+
+        function updateModel(e:ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+        async function onSubmit(e:ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+
+            console.log('userLogin:' + userLogin);
+        }
+
     return(
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth/>
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='passaword' fullWidth/>
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' label='usuario' variant='outlined' name='usuario' margin='normal' fullWidth/>
+                        <TextField value={userLogin.senha}  onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='passaword' fullWidth/>
                         <Box marginTop={2} textAlign='center'>
                             <Link to='/home' className='text-decorator-none'>
                                 <Button type='submit' variant='contained' color='primary'>
---
+++ src/services/Service.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+export const api = axios.create({
+    baseURL: '' /*VER ISSO AQUI PQ NÃO SEI PQ O DOCKER NÃO GOSTA DE MIM*/
+})
+
+export const  login = async(url:any, dados:any, setDado:any) =>{
+    const resposta  = await api.post(url, dados)
+    setDado(resposta.data)
+}
